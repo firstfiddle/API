be2c3195189a0a3f9f501956e39e12284de03634
onlinemath85@gmail.com <onlinemath85@gmail.com>
2024-05-17 01:20:47 -0700

    f

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,4 +1,4 @@
-const UserModel = require('../models/user')
+const UserModel = require('../models/User')
 const cloudinary = require('cloudinary');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
