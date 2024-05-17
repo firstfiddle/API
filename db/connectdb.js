7a844c5695d8532d755561da79195c7bc460a2f0
onlinemath85@gmail.com <onlinemath85@gmail.com>
2024-05-17 01:00:32 -0700

    first commit

---
+++ db/connectdb.js
@@ -0,0 +1,14 @@
+//mongodb connection
+
+const mongoose = require('mongoose')
+
+const connectDb = () => {
+    return mongoose.connect(process.env.LIVE_URL)
+        .then(() => {
+            console.log("connected successfully");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+module.exports = connectDb;
